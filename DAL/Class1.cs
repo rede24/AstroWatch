0e6a13197528bfd19dbaf1681e034eabd442bd8d
rede24 <57299466+rede24@users.noreply.github.com>
2021-09-14 12:15:00 +0300

    api dal unitesting

---
+++ DAL/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class Dal
+    {
+          const string APIKEY = "js4BJax4nac2gMLpPtK0IUEOHg5uDyPsLT5dcFGh";
+          string GetImageOfTheDayFromNASAApi()
+          {
+               string result = "";
+               string request = "https://api.nasa.gov/planetary/apod?api_key=js4BJax4nac2gMLpPtK0IUEOHg5uDyPsLT5dcFGh";
+
+
+
+
+
+
+               return result;
+
+          }
+    }
+}
---
+++ UnitTesting/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTesting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTesting")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0a56db15-0c29-477e-a196-b4d25b4a74b9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
