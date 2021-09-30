9395c80fda73c66738bd7b1d10ce8321b8a1221e
rede24 <57299466+rede24@users.noreply.github.com>
2021-10-01 01:34:28 +0300

    carusel impl

--- AstroWatch/Pages/SolarSystem.xaml.cs
+++ AstroWatch/Pages/SolarSystem.xaml.cs
@@ -24,5 +24,58 @@ namespace AstroWatch.Pages
           {
                InitializeComponent();
           }
+          #region Carousel
+
+          private void CarouselSpeedSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+          {
+               ExampleCarouselControl.RotationSpeed = e.NewValue;
+          }
+
+          private void LookdownOffsetSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+          {
+               ExampleCarouselControl.LookDownOffset = e.NewValue;
+               ExampleCarouselControl.SetElementPositions();
+          }
+
+          private void CarouselFadeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+          {
+               ExampleCarouselControl.Fade = e.NewValue;
+               ExampleCarouselControl.SetElementPositions();
+          }
+
+          private void CarouselScaleSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+          {
+               ExampleCarouselControl.Scale = e.NewValue;
+               ExampleCarouselControl.SetElementPositions();
+          }
+
+          private void VerticalOrientationRadioButton_Checked(object sender, RoutedEventArgs e)
+          {
+               ExampleCarouselControl.Width = 0;
+               ExampleCarouselControl.Height = 600;
+               ExampleCarouselControl.ReInitialize();
+          }
+
+          private void HorizontalOrientationRadioButton_Checked(object sender, RoutedEventArgs e)
+          {
+               ExampleCarouselControl.Width = 600;
+               ExampleCarouselControl.Height = 0;
+               ExampleCarouselControl.ReInitialize();
+          }
+
+          private void ExampleCarouselControl_OnElementSelected(object sender)
+          {
+               //spherecontrol selected = examplecarouselcontrol.currentlyselected as spherecontrol;
+               //if (currentlyselectedellipse != null)
+               //     currentlyselectedellipse.fill = selected.spherefill;
+               //if ((currentlyselectednametextblock != null) && (currentlyselectednameshadowtextblock != null))
+               //{
+               //     currentlyselectednametextblock.foreground = selected.spherefill;
+               //     currentlyselectednametextblock.text = selected.name;
+               //     currentlyselectednameshadowtextblock.text = selected.name;
+               //}
+          }
+
+          #endregion
      }
 }
