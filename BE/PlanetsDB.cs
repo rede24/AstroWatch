47068282746d2dc21baeefce5f802d72bc5a802e
rede24 <57299466+rede24@users.noreply.github.com>
2021-10-08 03:48:20 +0300

    ef

---
+++ BE/PlanetsDB.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+     public class Planet
+
+     {
+          public int ID { get; set; }
+          public string Name { get; set; }
+          public string Url { get; set; }
+
+          public string Description { get; set; }
+
+     }
+
+
+     public class PlanetDB : DbContext
+     {
+          public PlanetDB() : base("astrowatchdb")
+          {
+
+          }
+          public DbSet<Planet> SolarSystem { get; set; }
+          // public DbSet<Planet> SavedPlanets{ get; set; }
+
+
+
+
+     }
+
+
+
+
+}
